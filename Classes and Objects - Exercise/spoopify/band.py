3e27023c9416b4dbbf1fbe1ac4c3e8b68309e9b1
PetarSP <86008069+PetarSP@users.noreply.github.com>
2021-11-10 16:53:20 +0200

    OOP

--- Classes and Objects - Exercise/spoopify/band.py
+++ Classes and Objects - Exercise/spoopify/band.py
@@ -1,5 +1,3 @@
-from unittest import result
-
 from spoopify.album import Album
 
 
@@ -11,23 +9,24 @@ class Band:
     def add_album(self, new_album: Album):
         for album in self.albums:
             if album.name == new_album.name:
-                return f"Band {self.name} already has {new_album.name} in their library."
+                return f"Band {self.name} already has {album.name} in their library."
         self.albums.append(new_album)
         return f"Band {self.name} has added their newest album {new_album.name}."
 
     def remove_album(self, album_name: str):
-        for album in self.albums:
-            if album.published:
-                return f"Album has been published. It cannot be removed."
+
         for album in self.albums:
             if album.name == album_name:
-                self.albums.remove(album_name)
-                return f"Album {album_name} has been removed."
+                if album.published:
+                    return f"Album has been published. It cannot be removed."
+                else:
+                    self.albums.remove(album)
+                    return f"Album {album_name} has been removed."
         return f"Album {album_name} is not found."
 
     def details(self):
         result = f"Band {self.name}"
         for album in self.albums:
             result += "\n"
-            result += f"{album.details()}"
+            result += album.details()
         return result
--- Classes and Objects - Exercise/spoopify/main.py
+++ Classes and Objects - Exercise/spoopify/main.py
@@ -14,3 +14,4 @@ print(band.add_album(album))
 print(band.remove_album("Initial D"))
 print(band.details())
 
+
